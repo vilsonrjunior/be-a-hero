d2eca9c6554776ec1c982744508cc98ec01286f5
vilson junior <vilsonrjunior@gmail.com>
2020-04-14 09:11:12 +0100

    first changes on /profile page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logoImg from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import './style.css';
 
@@ -17,6 +17,95 @@ export default function Profile() {
                     <FiPower size={18} color="E02041" />
                 </button>
             </header>
+
+            <h1>Current requests</h1>
+
+            <ul>
+                <li>
+                    <strong>REQUEST:</strong>
+                    <p>Test</p>
+
+                    <strong>DESCRIPTION:</strong>
+                    <p>Test</p>
+
+                    <strong>AMOUNT:</strong>
+                    <p>£150.00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="A8A8B3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>REQUEST:</strong>
+                    <p>Test</p>
+
+                    <strong>DESCRIPTION:</strong>
+                    <p>Test</p>
+
+                    <strong>AMOUNT:</strong>
+                    <p>£150.00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="A8A8B3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>REQUEST:</strong>
+                    <p>Test</p>
+
+                    <strong>DESCRIPTION:</strong>
+                    <p>Test</p>
+
+                    <strong>AMOUNT:</strong>
+                    <p>£150.00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="A8A8B3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>REQUEST:</strong>
+                    <p>Test</p>
+
+                    <strong>DESCRIPTION:</strong>
+                    <p>Test</p>
+
+                    <strong>AMOUNT:</strong>
+                    <p>£150.00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="A8A8B3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>REQUEST:</strong>
+                    <p>Test</p>
+
+                    <strong>DESCRIPTION:</strong>
+                    <p>Test</p>
+
+                    <strong>AMOUNT:</strong>
+                    <p>£150.00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="A8A8B3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>REQUEST:</strong>
+                    <p>Test</p>
+
+                    <strong>DESCRIPTION:</strong>
+                    <p>Test</p>
+
+                    <strong>AMOUNT:</strong>
+                    <p>£150.00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="A8A8B3" />
+                    </button>
+                </li>
+            </ul>
         </div>
     )
 }
